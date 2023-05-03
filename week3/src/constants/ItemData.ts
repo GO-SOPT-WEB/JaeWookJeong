d367cda5be01c95ee71e52166399a1a51ccc1db2
Dev-jwJeong <rgn95652@gmail.com>
2023-05-04 03:33:27 +0900

    feat : 국가 카드 상수 데이터

---
+++ week3/src/constants/ItemData.ts
@@ -0,0 +1,61 @@
+import {
+  Brazil,
+  China,
+  Germany,
+  Japan,
+  Korea,
+  North,
+  Spain,
+  Uk,
+  Usa,
+} from "../assets";
+
+const ITEM_DATA = [
+  {
+    id: 1,
+    src: Brazil,
+    alt: "brazil",
+  },
+  {
+    id: 2,
+    src: China,
+    alt: "china",
+  },
+  {
+    id: 3,
+    src: Germany,
+    alt: "germany",
+  },
+  {
+    id: 4,
+    src: Japan,
+    alt: "japan",
+  },
+  {
+    id: 5,
+    src: Korea,
+    alt: "korea",
+  },
+  {
+    id: 6,
+    src: North,
+    alt: "north",
+  },
+  {
+    id: 7,
+    src: Spain,
+    alt: "spain",
+  },
+  {
+    id: 8,
+    src: Uk,
+    alt: "uk",
+  },
+  {
+    id: 9,
+    src: Usa,
+    alt: "usa",
+  },
+];
+
+export default ITEM_DATA;
