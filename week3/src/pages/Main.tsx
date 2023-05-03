192bc1a7987427415679af71e6ca306e761f3864
Dev-jwJeong <rgn95652@gmail.com>
2023-05-04 02:54:53 +0900

    feat : 단계에 따른 문제 수 수정 함수

--- week3/src/pages/Main.tsx
+++ week3/src/pages/Main.tsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import { Header, Nav } from "../components";
 
 const Main = () => {
+  const [count, setCount] = useState(5);
+  const handleChangeCount = (mode: string) => {
+    switch (mode) {
+      case "EASY":
+        setCount(5);
+        break;
+      case "NORMAL":
+        setCount(7);
+        break;
+      case "HARD":
+        setCount(9);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <>
-      <Header />
-      <Nav />
+      <Header count={count} />
+      <Nav handleChangeCount={handleChangeCount} />
     </>
   );
 };
