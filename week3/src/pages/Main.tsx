22b4190b328c2cee82b350300d9eafa52da83600
Dev-jwJeong <rgn95652@gmail.com>
2023-05-05 06:28:44 +0900

    feat : 게임이 끝났을 때 모달 적용

--- week3/src/pages/Main.tsx
+++ week3/src/pages/Main.tsx
@@ -1,13 +1,16 @@
-import { useMemo, useState } from "react";
+import styled from "styled-components";
+import { useEffect, useMemo, useState } from "react";
 import { Header, ItemList, Nav } from "../components";
 import ITEM_DATA from "../constants/ItemData";
 import { itemDataProps } from "../types/cardList";
+import { FinalModal, Modal } from "../components/common";
 
 const Main = () => {
   const [count, setCount] = useState(5);
   const [score, setScore] = useState(0);
   const [isClear, setIsClear] = useState(false);
   const [isFliped, setIsFliped] = useState<number[]>([]);
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
   let randomDataList: itemDataProps[] = [];
   let pasteDataList: itemDataProps[] = [];
@@ -17,6 +20,12 @@ const Main = () => {
   const realDataList: itemDataProps[] = useMemo(() => {
     return pasteDataList.sort(() => Math.random() - 0.5);
   }, [isClear]);
+
+  useEffect(() => {
+    if (score === count) {
+      setIsOpenModal((prev) => !prev);
+    }
+  }, [score, count]);
 
   const handleChangeCount = (mode: string) => {
     setScore(0);
@@ -37,7 +46,7 @@ const Main = () => {
   };
 
   return (
-    <>
+    <Wrapper>
       <Header
         count={count}
         score={score}
@@ -53,8 +62,17 @@ const Main = () => {
         isFliped={isFliped}
         setIsFliped={setIsFliped}
       />
-    </>
+      {isOpenModal && (
+        <Modal>
+          <FinalModal setIsOpenModal={setIsOpenModal} />
+        </Modal>
+      )}
+    </Wrapper>
   );
 };
 
 export default Main;
+
+const Wrapper = styled.main`
+  position: relative;
+`;
