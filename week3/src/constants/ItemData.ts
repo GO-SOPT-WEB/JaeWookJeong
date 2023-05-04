4782bec2822a022b23a5ced6b6e2426e03929b69
Dev-jwJeong <rgn95652@gmail.com>
2023-05-05 05:47:07 +0900

    feat : 같은 종류 카드 구분 키 추가

--- week3/src/constants/ItemData.ts
+++ week3/src/constants/ItemData.ts
@@ -16,46 +16,55 @@ const ITEM_DATA: itemDataProps[] = [
     id: 1,
     src: Brazil,
     alt: "brazil",
+    card: 1,
   },
   {
     id: 2,
     src: China,
     alt: "china",
+    card: 2,
   },
   {
     id: 3,
     src: Germany,
     alt: "germany",
+    card: 3,
   },
   {
     id: 4,
     src: Japan,
     alt: "japan",
+    card: 4,
   },
   {
     id: 5,
     src: Korea,
     alt: "korea",
+    card: 5,
   },
   {
     id: 6,
     src: North,
     alt: "north",
+    card: 6,
   },
   {
     id: 7,
     src: Spain,
     alt: "spain",
+    card: 7,
   },
   {
     id: 8,
     src: Uk,
     alt: "uk",
+    card: 8,
   },
   {
     id: 9,
     src: Usa,
     alt: "usa",
+    card: 9,
   },
 ];
 
