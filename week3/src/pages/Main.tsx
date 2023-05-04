317e074f2b2f139d64e4c6bf5030228023d5d0a6
Dev-jwJeong <rgn95652@gmail.com>
2023-05-05 05:48:21 +0900

    useMemo를 활용해서 재랜더링 방지

--- week3/src/pages/Main.tsx
+++ week3/src/pages/Main.tsx
@@ -1,41 +1,58 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { Header, ItemList, Nav } from "../components";
 import ITEM_DATA from "../constants/ItemData";
 import { itemDataProps } from "../types/cardList";
 
 const Main = () => {
   const [count, setCount] = useState(5);
+  const [score, setScore] = useState(0);
   const [isClear, setIsClear] = useState(false);
+  const [isFliped, setIsFliped] = useState<number[]>([]);
+
   let randomDataList: itemDataProps[] = [];
   let pasteDataList: itemDataProps[] = [];
-
-  randomDataList = ITEM_DATA.sort(() => Math.random() - 0.5);
+  randomDataList = ITEM_DATA.slice(0, count).sort(() => Math.random() - 0.5);
   pasteDataList = [...randomDataList, ...randomDataList];
 
+  const realDataList: itemDataProps[] = useMemo(() => {
+    return pasteDataList.sort(() => Math.random() - 0.5);
+  }, [isClear]);
+
   const handleChangeCount = (mode: string) => {
+    setScore(0);
+    setIsClear(true);
     switch (mode) {
       case "EASY":
         setCount(5);
-        setIsClear(true);
         break;
       case "NORMAL":
         setCount(7);
-        setIsClear(true);
         break;
       case "HARD":
         setCount(9);
-        setIsClear(true);
         break;
       default:
-        break;
+        setCount(5);
     }
   };
 
   return (
     <>
-      <Header count={count} />
+      <Header
+        count={count}
+        score={score}
+        handleChangeCount={handleChangeCount}
+      />
       <Nav handleChangeCount={handleChangeCount} />
-      <ItemList count={count} pasteDataList={pasteDataList} isClear={isClear} />
+      <ItemList
+        count={count}
+        realDataList={realDataList}
+        isClear={isClear}
+        score={score}
+        setScore={setScore}
+        isFliped={isFliped}
+        setIsFliped={setIsFliped}
+      />
     </>
   );
 };
