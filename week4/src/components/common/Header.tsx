c4b9c469e352c94f29752a2d9f425f3abfe81d79
Dev-jwJeong <rgn95652@gmail.com>
2023-05-11 12:58:15 +0900

    style : 헤더 컴포넌트 스타일링

--- week4/src/components/common/Header.tsx
+++ week4/src/components/common/Header.tsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
 const Header = () => {
-  return <div>헤더</div>;
+  return <StWrapper>날씨 예보</StWrapper>;
 };
 
 export default Header;
+
+const StWrapper = styled.header`
+  width: 100%;
+  padding: 5rem;
+
+  background-color: ${({ theme }) => theme.colors.Color_Sky};
+  color: ${({ theme }) => theme.colors.Color_White};
+  ${({ theme }) => theme.fonts.Noto_B_Title_3}
+`;
