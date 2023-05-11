f9791250b95f0c6568803a9d85d8d4283a3e6f7a
Dev-jwJeong <rgn95652@gmail.com>
2023-05-11 13:30:45 +0900

    feat : 일단/주간 옵션 타입 정의

---
+++ week4/src/constants/option.ts
@@ -0,0 +1,14 @@
+interface OptionInfo {
+  value: string;
+  text: string;
+}
+export const OPTION: OptionInfo[] = [
+  {
+    value: "day",
+    text: "오늘",
+  },
+  {
+    value: "week",
+    text: "주간",
+  },
+];
