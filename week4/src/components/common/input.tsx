96da44c0059a228c032e187c3b58391563720ada
Dev-jwJeong <rgn95652@gmail.com>
2023-05-11 13:31:37 +0900

    feat : option에 따른 routing 처리

--- week4/src/components/common/input.tsx
+++ week4/src/components/common/input.tsx
@@ -1,5 +1,57 @@
-const input = () => {
-  return <div>input</div>;
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { OPTION } from "../../constants/option";
+import { useEffect, useState } from "react";
+
+const Input = () => {
+  const [selected, setSelected] = useState("day");
+  const [city, setCity] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setCity("");
+  }, [selected]);
+
+  const handleOption = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelected(e.target.value);
+  };
+
+  const handleInputCity = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCity(e.target.value);
+  };
+
+  const handleNaviagateInput = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate(`/${selected}/${city}`);
+    setCity("");
+  };
+
+  return (
+    <StWrapper>
+      <StForm onSubmit={handleNaviagateInput}>
+        <StSelect onChange={handleOption} value={selected}>
+          {OPTION.map(({ value, text }, idx) => (
+            <option key={idx} value={value}>
+              {text}
+            </option>
+          ))}
+        </StSelect>
+        <StInput
+          type="text"
+          value={city}
+          placeholder="도시를 입력해주세요"
+          onChange={handleInputCity}
+        />
+        <StBtn type="submit">날씨 검색</StBtn>
+      </StForm>
+    </StWrapper>
+  );
 };
 
-export default input;
+export default Input;
+
+const StWrapper = styled.section``;
+const StForm = styled.form``;
+const StSelect = styled.select``;
+const StInput = styled.input``;
+const StBtn = styled.button``;
