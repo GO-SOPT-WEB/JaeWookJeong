9c84866b381f2ec219b7a1656c839a88e4da2aca
Dev-jwJeong <rgn95652@gmail.com>
2023-05-12 11:07:12 +0900

    feat : 존재하지 않는 주소 에러 분기 처리

--- week4/src/components/common/Router.tsx
+++ week4/src/components/common/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Main, TodayWeather, WeeklyWeather } from "../../pages";
+import { Error } from ".";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />}>
+          <Route path="*" element={<Error />} />
           <Route path="day/:area" element={<TodayWeather />} />
           <Route path="week/:area" element={<WeeklyWeather />} />
         </Route>
