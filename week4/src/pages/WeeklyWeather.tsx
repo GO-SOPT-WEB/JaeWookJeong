37dc4c6cc4bbc3884ab02fd369f0ba48894bce3a
Dev-jwJeong <rgn95652@gmail.com>
2023-05-12 10:55:57 +0900

    feat : 날짜별 날씨 렌더링

--- week4/src/pages/WeeklyWeather.tsx
+++ week4/src/pages/WeeklyWeather.tsx
@@ -1,9 +1,29 @@
+import { useParams } from "react-router-dom";
+import { WeatherItem } from "../components";
 import { WeatherLayout } from "../components/Layout";
+import useGetWeeklyWeather from "../lib/hooks/useGetWeeklyWeather";
+import { LandingItem } from "../components/Landing";
 
 const WeeklyWeather = () => {
+  const landingArr: string[] = Array(5).fill("");
+  const spliceDate = [0];
+  const { area } = useParams() as { area: string };
+  const { weatherInfo, isLoading } = useGetWeeklyWeather(area);
+
+  if (weatherInfo) {
+    const len = weatherInfo?.list.length / 5;
+    for (let i = 0; i < 4; i++) {
+      spliceDate.push(spliceDate[i] + len);
+    }
+  }
+
   return (
     <WeatherLayout>
-      <div>1</div>
+      {isLoading && landingArr.map((_, idx) => <LandingItem key={idx} />)}
+      {weatherInfo &&
+        spliceDate.map((weather, idx) => (
+          <WeatherItem key={idx} weatherInfo={weatherInfo.list[weather]} />
+        ))}
     </WeatherLayout>
   );
 };
